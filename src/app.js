3e3d37872163e9668efa62296611e02f32ff31cc
leotricotti <tricottileo@gmail.com>
2023-08-24 22:04:21 -0300

    commit

--- src/app.js
+++ src/app.js
@@ -4,9 +4,6 @@ import passport from "passport";
 import * as dotenv from "dotenv";
 import __dirname from "./utils.js";
 import { Server } from "socket.io";
-import session from "express-session";
-import MongoStore from "connect-mongo";
-import cookieParser from "cookie-parser";
 import handlebars from "express-handlebars";
 import CartsRouter from "./routes/carts.routes.js";
 import LoginRouter from "./routes/login.routes.js";
@@ -21,6 +18,7 @@ import RealTimeProducts from "./routes/realTimeProducts.routes.js";
 //Inicializar servicios
 dotenv.config();
 const productsManager = new Products();
+initializePassport();
 
 //Variables
 const app = express();
@@ -76,47 +74,14 @@ app.use(express.static("public"));
 app.use(cookieParser("C0d3rS3cr3t"));
 app.use(express.urlencoded({ extended: true }));
 
-//Middleware para hacer privadas las rutas
-const auth = async (req, res, next) => {
-  try {
-    const session = await req.session;
-    const user = await session.user;
-    if (user && session) {
-      return next();
-    } else {
-      return res.status(401).json({
-        respuesta: "No estás autorizado",
-      });
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-// Middleware para validar si el usuario es administrador
-const authAdmin = async (req, res, next) => {
-  try {
-    const admin = req.session.admin;
-    if (admin) {
-      return next();
-    } else {
-      return res.status(401).json({
-        respuesta: "No estás autorizado",
-      });
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 // Routes
 app.use("/", LoginRouter);
 app.use("/signup", SignUpRouter);
 app.use("/forgot", ForgotRouter);
-app.use("/api/carts", auth, CartsRouter);
+app.use("/api/carts", CartsRouter);
 app.use("/api/session", SessionRouter);
-app.use("/api/products", auth, ProductsRouter);
-app.use("/api/realtimeproducts", authAdmin, RealTimeProducts);
+app.use("/api/products", ProductsRouter);
+app.use("/api/realtimeproducts", RealTimeProducts);
 
 // Server
 const httpServer = app.listen(PORT, () => {
